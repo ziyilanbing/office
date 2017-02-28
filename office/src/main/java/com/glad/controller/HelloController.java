1c4e2c9a0123dc142ee73fed800a5d13f08006e8
zhongqs <529553014@qq.com>
2017-02-28 22:37:42 +0800

    controller change

--- office/src/main/java/com/glad/controller/HelloController.java
+++ office/src/main/java/com/glad/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.glad.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,8 +16,22 @@ import com.glad.model.HelloModel;
 @SessionAttributes("helloModel")
 public class HelloController {
 
+	@ModelAttribute("loginModel")
+	public HelloModel createLoginModel() {
+		HelloModel loginModel = new HelloModel();
+		return loginModel;
+	}
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String helloWorld(Model model, @ModelAttribute("helloModel") HelloModel helloModel) {
+	public String initModel(@ModelAttribute("helloModel") HelloModel loginModel) {
+		System.out.println("aaaa");
+		return "hello";
+	}
+
+	@RequestMapping(value = { "/init", "complete" }, method = RequestMethod.GET)
+	public String helloWorld(ModelMap map, HelloModel helloModel,
+			HttpServletRequest request) {
+		System.out.println("aaaaa");
 		helloModel.setMessage("abc");
 		return "hello";
 	}
