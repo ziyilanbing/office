04001fa2a8cc8cc844dbf2dd645710c90e14866a
Your Correct Name <your-correct-email@example.com>
2017-04-11 22:56:15 +0800

    add bat file

---
+++ sql/executeSql.bat
@@ -0,0 +1,6 @@
+
+cd C:\Program Files\MySQL\MySQL Server 5.7\bin\
+
+mysql -u root -p123456 <C:\Dev\workspace\office\sql\createSchema.sql 
+
+mysql -u root -p123456 office <C:\Dev\workspace\office\sql\security.sql
