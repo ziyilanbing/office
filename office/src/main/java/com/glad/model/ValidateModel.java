ca16581062990ae7e55d57bcbdd5972788b42e15
guoy <guoy@guoy-PC.glad.com>
2017-04-12 09:10:19 +0800

    ·

--- office/src/main/java/com/glad/model/ValidateModel.java
+++ office/src/main/java/com/glad/model/ValidateModel.java
@@ -1,11 +1,14 @@
 package com.glad.model;
 
+import javax.validation.constraints.Size;
+
 import com.glad.annotation.NotEmpty;
 import com.glad.component.AbstractModel;
 
 public class ValidateModel extends AbstractModel {
 
 	@NotEmpty
+	@Size(max = 8, min = 0)
 	private String textInput;
 
 	public String getTextInput() {
