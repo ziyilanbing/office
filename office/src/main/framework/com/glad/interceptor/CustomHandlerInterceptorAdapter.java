b688f30373711e926bff687a29b02e3295388526
zhongqishang <zhongqs@ZHONGQS-PC.glad.com>
2017-04-13 16:09:46 +0800

    `

--- office/src/main/framework/com/glad/interceptor/CustomHandlerInterceptorAdapter.java
+++ office/src/main/framework/com/glad/interceptor/CustomHandlerInterceptorAdapter.java
@@ -2,15 +2,23 @@ package com.glad.interceptor;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.request.WebRequestInterceptor;
 
+import com.glad.aspect.TraceAdvice;
 import com.glad.util.ScreenUtils;
 
 public class CustomHandlerInterceptorAdapter implements WebRequestInterceptor {
+
+	/**
+	 * slf4j logger
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(TraceAdvice.class);
 
 	@Override
 	public void preHandle(WebRequest request) throws Exception {
@@ -19,21 +27,21 @@ public class CustomHandlerInterceptorAdapter implements WebRequestInterceptor {
 
 	@Override
 	public void postHandle(WebRequest request, ModelMap model) throws Exception {
-		for (String key : model.keySet()) {
 
-			Object object = model.get(key);
-
-			if (object instanceof BindingResult) {
-				List<FieldError> fields = ((BindingResult) object).getFieldErrors();
-				if (fields.size() > 0) {
-					String code = fields.get(0).getCode();
-					String message = fields.get(0).getDefaultMessage();
-					model.put(ScreenUtils.DATA_KEY, message);
-					break;
+		if (model != null) {
+			for (String key : model.keySet()) {
+				Object object = model.get(key);
+				if (object instanceof BindingResult) {
+					List<FieldError> fields = ((BindingResult) object).getFieldErrors();
+					if (fields.size() > 0) {
+						String code = fields.get(0).getCode();
+						String message = fields.get(0).getDefaultMessage();
+						model.put(ScreenUtils.DATA_KEY, message);
+						break;
+					}
 				}
 			}
 		}
-
 	}
 
 	@Override
