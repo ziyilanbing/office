2c25e876ec4cba42f81012f7753ef5c78a50cc46
zhongqishang <zhongqs@ZHONGQS-PC.glad.com>
2017-05-12 14:13:36 +0800

    add menu

--- office/src/main/framework/com/glad/taglib/MenuTag.java
+++ office/src/main/framework/com/glad/taglib/MenuTag.java
@@ -1,6 +1,7 @@
 package com.glad.taglib;
 
 import java.io.Serializable;
+import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
@@ -15,7 +16,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.servlet.tags.HtmlEscapeTag;
 
 import com.glad.Constants;
+import com.glad.entity.SysMenu;
 import com.glad.menu.MenuTree;
+import com.glad.menu.TreeNode;
 import com.glad.service.MenuService;
 
 /**
@@ -31,7 +34,7 @@ public class MenuTag extends HtmlEscapeTag {
 	 * slf4j logger
 	 */
 	private static final Logger logger = LoggerFactory.getLogger(MenuTag.class);
-
+	private static String MENU_LEVEL_HTML = "aaaaaaaaaaaaaaaaaaaaa";
 	@Autowired
 	private MenuService menuService;
 
@@ -71,11 +74,73 @@ public class MenuTag extends HtmlEscapeTag {
 		return EVAL_PAGE;
 	}
 
+	/**
+	 * 
+	 * @param menuTree
+	 * @return
+	 */
 	private StringBuilder makeHtml(MenuTree menuTree) {
-		// TODO
-		StringBuilder builder = new StringBuilder("aaaaa");
+		StringBuilder builder = new StringBuilder();
+		// add menu's top [Glad Office]
+		builder.append("<div class=\"navbar-default sidebar\" role=\"navigation\">\r\n");
+		builder.append("  <div class=\"sidebar-nav navbar-collapse\">\r\n");
+		builder.append("    <ul class=\"nav\" id=\"side-menu\">\r\n");
+		builder.append("      <li><a href=\"#\"><i class=\"fa fa-dashboard fa-fw\"></i> Dashboard</a></li>\r\n");
+
+		builder.append(makeListHtml(menuTree.getRoot(), 1));
+
+		builder.append("this is menu ended");
 
 		return builder;
+	}
+
+	private String makeListHtml(TreeNode node, int level) {
+		StringBuilder menuHtml = new StringBuilder();
+
+		Iterator childIterator = node.getChildIterator();
+		while (childIterator.hasNext()) {
+			TreeNode treeNode = (TreeNode) childIterator.next();
+			menuHtml.append(makeLiHtml(treeNode, level));
+
+			if (treeNode.hasChild()) {
+				level++;
+				menuHtml.append(ulLevelHtml(level));
+				menuHtml.append(makeListHtml(treeNode, level));
+				menuHtml.append("</ul>\r\n");
+				level--;
+			}
+			menuHtml.append("</li>\r\n");
+
+		}
+		return menuHtml.toString();
+	}
+
+	private Object ulLevelHtml(int level) {
+		StringBuilder ulLvlHtml = new StringBuilder();
+		if (level == 2) {
+			ulLvlHtml.append("      <ul class=\"nav nav-second-level collapse\">\r\n");
+		} else {
+			ulLvlHtml.append("      <ul class=\"nav nav-third-level collapse\">\r\n");
+		}
+		return ulLvlHtml.toString();
+	}
+
+	private String makeLiHtml(TreeNode treeNode, int level) {
+
+		StringBuilder liHtml = new StringBuilder();
+		liHtml.append("        <li><a href=\"");
+		liHtml.append(((SysMenu) treeNode.getData()).getMenuUrl());
+		if (level == 1) {
+			liHtml.append("\"><i class=\"fa fa-sitemap fa-fw\"></i>");
+		} else {
+			liHtml.append("\">");
+		}
+		liHtml.append(((SysMenu) treeNode.getData()).getMenuName());
+		if (level == 1) {
+			liHtml.append("<span class=\"fa arrow\"></span>");
+		}
+		liHtml.append("</a>");
+		return liHtml.toString();
 	}
 
 	private class MenuHtmlData implements Serializable {
