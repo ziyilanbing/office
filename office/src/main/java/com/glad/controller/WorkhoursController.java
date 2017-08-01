e9b248912167c9204ef4bc3b16e7bc8229fdd1a4
zhongqishang <529553014@qq.com>
2017-08-01 17:39:12 +0800

    update

--- office/src/main/java/com/glad/controller/WorkhoursController.java
+++ office/src/main/java/com/glad/controller/WorkhoursController.java
@@ -1,5 +1,8 @@
 package com.glad.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.glad.annotation.ScreenId;
@@ -21,24 +26,38 @@ import com.glad.service.WorkhoursService;
  */
 @Controller
 @RequestMapping("/workhours")
-@SessionAttributes("workhoursModel")
+@SessionAttributes("workhoursModel") // session model
 @ScreenId("glad.office.workhours")
 public class WorkhoursController {
 
 	@Autowired
 	private WorkhoursService workhoursService;
 
+	/**
+	 * session model
+	 * 
+	 * @return
+	 */
 	@ModelAttribute("workhoursModel")
 	public WorkhoursModel createWorkhoursModel() {
 		return new WorkhoursModel();
 	}
 
 	@RequestMapping(value = {"/*"}, method = {RequestMethod.GET})
-	public String init(ModelMap model) throws Exception {
-
+	public String init(ModelMap model, @ModelAttribute WorkhoursModel workhoursModel) throws Exception {
+		workhoursModel = new WorkhoursModel();
 		return "workhours/register";
 	}
 
+	/**
+	 * Post demo
+	 * 
+	 * @param model
+	 * @param workhoursModel
+	 * @param result
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(value = {"/confirm"}, method = {RequestMethod.POST})
 	public String confirm(ModelMap model, @Valid @ModelAttribute WorkhoursModel workhoursModel, BindingResult result) throws Exception {
 
@@ -51,6 +70,31 @@ public class WorkhoursController {
 		workhoursModel.setOdhWktmManageChecked(odhWktmManage);
 
 		return "workhours/register";
+	}
+
+	/**
+	 * Ajax Post Demo
+	 * 
+	 * @param model
+	 * @param aaa
+	 * @param workhoursModel
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/confirmajax")
+	@ResponseBody
+	public Map<String, Object> confirmAjax(ModelMap model, @RequestBody Map<String, Object> aaa, @ModelAttribute WorkhoursModel workhoursModel)
+		throws Exception {
+
+		Map<String, Object> rtnmap = new HashMap<String, Object>();
+		OdhWktmManage odhWktmManage = new OdhWktmManage();
+		odhWktmManage.setUserNo("00734");
+		odhWktmManage.setRecNo(0001);
+		odhWktmManage.setMemo(workhoursModel.getComment());
+
+		workhoursModel.setComment("");
+		workhoursModel.setOdhWktmManageChecked(odhWktmManage);
+		return rtnmap;
 	}
 
 	@RequestMapping(value = {"/submit"}, method = {RequestMethod.POST})
