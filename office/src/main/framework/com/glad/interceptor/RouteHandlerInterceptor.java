fd87d0bfe4403e8f129567cdfafdbc286005a8b7
zhongqishang <529553014@qq.com>
2017-08-21 10:05:53 +0800

    add logger

--- office/src/main/framework/com/glad/interceptor/RouteHandlerInterceptor.java
+++ office/src/main/framework/com/glad/interceptor/RouteHandlerInterceptor.java
@@ -3,19 +3,27 @@ package com.glad.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.glad.base.BaseController;
+
 public class RouteHandlerInterceptor implements HandlerInterceptor {
+
+	protected Logger logger = LoggerFactory.getLogger(BaseController.class);
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		// PJAX
 		if (request.getHeader("X-PJAX") != null) {
+			logger.info("Request X-PJAX");
 			return true;
 		}
 		String contextPath = request.getContextPath();
 		response.sendRedirect(contextPath + "/dashboard/init?redirectby=" + request.getRequestURI());
+		logger.info("Redirect : " + request.getRequestURI());
 		return false;
 	}
 
