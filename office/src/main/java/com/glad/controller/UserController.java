4255bfdd238042daa0f3be72cbd0f384d505b7c5
zhongqishang <529553014@qq.com>
2017-08-31 17:41:21 +0800

    add menu to json

--- office/src/main/java/com/glad/controller/UserController.java
+++ office/src/main/java/com/glad/controller/UserController.java
@@ -1,12 +1,21 @@
 package com.glad.controller;
 
+import java.util.Iterator;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.glad.Constants;
 import com.glad.annotation.ScreenId;
 import com.glad.base.BaseController;
+import com.glad.entity.OdhModlInfo;
+import com.glad.menu.MenuTree;
+import com.glad.menu.TreeNode;
 import com.glad.model.DefaultModel;
 
 /**
@@ -21,6 +30,31 @@ public class UserController extends BaseController<DefaultModel> {
 	public String details(ModelMap model) throws Exception {
 
 		return getIndexView();
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/listjson", method = {RequestMethod.GET})
+	public String listjson(ModelMap model, HttpServletRequest request) throws Exception {
+		MenuTree menuTree = (MenuTree) request.getSession().getAttribute(Constants.USER_MENU_TREE);
+		System.out.println(menuToJson(menuTree.getRoot()));
+		return menuToJson(menuTree.getRoot());
+	}
+
+	private String menuToJson(TreeNode node) {
+		StringBuilder jsonsb = new StringBuilder("[");
+		Iterator childIterator = node.getChildIterator();
+		while (childIterator.hasNext()) {
+			TreeNode treeNode = (TreeNode) childIterator.next();
+			jsonsb.append("{layer:\"" + ((OdhModlInfo) treeNode.getData()).getModelId() + "\"");
+			jsonsb.append(",id:\"" + ((OdhModlInfo) treeNode.getData()).getModelId() + "\"");
+			jsonsb.append(",text:\"" + ((OdhModlInfo) treeNode.getData()).getModelName() + "\"");
+			jsonsb.append(",icon:\"fa fa-sitemap\"");
+			jsonsb.append(",children:");
+			jsonsb.append(menuToJson(treeNode));
+			jsonsb.append("},");
+		}
+		jsonsb.append("]");
+		return jsonsb.toString();
 	}
 
 }
